c6ee978408265f08802fc0544a8f02fbb4e953fd
filipecaldeira <filipecaldeira@fundep.com.br>
2025-09-25 16:53:11 -0300

    Melhorias

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom'; 
 import HomePage from './components/HomePage';
+import GerenciarEventosPage from './pages/GerenciarEventosPage'; 
 import './App.css';
 
 const App: React.FC = () => {
   return (
-    <HomePage />
+    // 3. Defina as rotas aqui dentro
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/gerenciar-eventos" element={<GerenciarEventosPage />} />
+    </Routes>
   );
 };
 
